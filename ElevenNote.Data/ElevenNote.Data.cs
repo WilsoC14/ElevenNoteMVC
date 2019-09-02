50f89371fac772dadaea6de1addce0138231a8df
WilsoC14 <dotwilsoncasey@gmail.com>
2019-09-02 14:07:33 -0400

    Created Category Class & Migrated Db

--- ElevenNote.Data/ElevenNote.Data.cs
+++ ElevenNote.Data/ElevenNote.Data.cs
@@ -33,6 +33,8 @@ namespace ElevenNote.Data
         }
 
         public DbSet<Note> Notes { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             modelBuilder
